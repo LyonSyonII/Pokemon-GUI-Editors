4a189d38174b32c2e4143e8fcaa6151261bb1b65
LyonSyonII <slimystudios@protonmail.com>
2022-05-15 13:08:41 +0200

    Added script builds

--- build_all.sh
+++ build_all.sh
@@ -1,14 +1,12 @@
 #! /usr/bin/env bash
 # Build moves
-pyinstaller -F -n moves_editor moves/src/app.py
-cp moves/dist/moves_editor moves_editor
-
-wineconsole pyinstaller -F -n moves_editor.exe moves/src/app.py
-cp moves/dist/moves_editor.exe moves_editor.exe
+pyinstaller -F -n moves_editor moves/src/app.py &
+wineconsole pyinstaller -F -n moves_editor.exe moves/src/app.py &
 
 # Build pokemon
-# pyinstaller -F -n pokemon_editor pokemon/src/app.py
-# cp pokemon/dist/pokemon_editor pokemon_editor
+# pyinstaller -F -n pokemon_editor pokemon/src/app.py &
 # 
-# wineconsole pyinstaller -F -n pokemon_editor.exe pokemon/src/app.py
-# cp pokemon/dist/pokemon_editor.exe pokemon_editor.exe
+# wineconsole pyinstaller -F -n pokemon_editor.exe pokemon/src/app.py &
+
+wait
+echo "Done!"
